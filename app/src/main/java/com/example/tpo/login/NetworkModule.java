14a882aa30a9768f442ba8f6ea2df6a4c96de784
JuanElliff <elliffjuan3@gmail.com>
2026-09-22 10:38:58 -0300

    Limpiar configuración temporal de pruebas locales

--- app/src/main/java/com/example/tpo/login/NetworkModule.java
+++ app/src/main/java/com/example/tpo/login/NetworkModule.java
@@ -18,8 +18,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NetworkModule {
 
     private static final String BASE_URL = "https://ronda-api-production.up.railway.app/";
-//private static final String BASE_URL = "http://10.0.2.2:8000/";
-
     @Provides
     @Singleton
     public OkHttpClient provideOkHttp(TokenManager tokenManager) {
